d4148d76f34fd6fcbcd870485eb143545475e475
joshuamendez <joshuafmendez@gmail.com>
2021-07-05 19:14:40 -0400

    implemented add new trip feature

--- client/src/Components/AddTrip.jsx
+++ client/src/Components/AddTrip.jsx
@@ -1,15 +1,54 @@
+import { useContext, useState } from "react";
+import TripsRater from "../apis/TripsRater";
+import { TripsStore } from "../store/TripsStore";
+
 const AddTrip = () => {
+  const { addTrip } = useContext(TripsStore);
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [priceRange, setPriceRange] = useState("Price Range");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const {data} = await TripsRater.post("/", {
+        name,
+        location,
+        price_range: priceRange,
+      });
+      addTrip(data.trip);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="d-flex mb-4 justify-content-center">
       <form className="row">
         <div className="col-sm-3">
-          <input type="text" className="form-control" placeholder="name" />
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            className="form-control"
+            placeholder="name"
+          />
         </div>
         <div className="col-sm-3">
-          <input type="text" className="form-control" placeholder="location" />
+          <input
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            type="text"
+            className="form-control"
+            placeholder="location"
+          />
         </div>
         <div className="col-sm-3">
-          <select className="form-select" id="inputGroupSelect04">
+          <select
+            value={priceRange}
+            onChange={(e) => setPriceRange(e.target.value)}
+            className="form-select"
+            id="inputGroupSelect04"
+          >
             <option disabled>Price Range</option>
             <option value="1">$</option>
             <option value="2">$$</option>
@@ -19,7 +58,13 @@ const AddTrip = () => {
           </select>
         </div>
         <div className="col-auto">
-          <button className="btn btn-primary">Add</button>
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="btn btn-primary"
+          >
+            Add
+          </button>
         </div>
       </form>
     </div>
--- client/src/store/TripsStore.js
+++ client/src/store/TripsStore.js
@@ -4,8 +4,11 @@ export const TripsStore = createContext();
 
 export const TripsStoreProvider = (props) => {
   const [trips, setTrips] = useState([]);
+  const addTrip = (trip) => {
+    setTrips([...trips, trip]);
+  }
   return (
-    <TripsStore.Provider value={{ trips, setTrips }}>
+    <TripsStore.Provider value={{ trips, setTrips, addTrip }}>
       {props.children}
     </TripsStore.Provider>
   );
